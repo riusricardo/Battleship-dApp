a6db530312a3ea62e68c1d3ed93c20878397d2ec
Ricardo Rius <data.tm@outlook.com>
2018-07-27 18:04:28 -0500

    geth-dev explicit

--- scripts/run_geth_dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -fR ./devNet/datadir/geth/
-
-geth --networkid=1337 --datadir=.devNet/datadir --password=config/development/password --port=30303 --rpc --rpcport=9545 --rpcaddr=localhost  --rpccorsdomain="*" --nodiscover --maxpeers=0 --ws --wsport=9546 --wsaddr=localhost --wsorigins="*" --mine --shh --rpcapi=eth,web3,net,debug,shh,personal --wsapi=eth,web3,net,shh,debug,personal --dev --dev.period 1 --targetgaslimit=4600000 --verbosity 6
